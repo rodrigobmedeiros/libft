891840c96876aeec9bc41be951aed37415bcc3df
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2023-10-17 18:29:09 -0300

    Add first function to my repo

---
+++ libft.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: robernar <robernar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/16 23:08:08 by robernar          #+#    #+#             */
+/*   Updated: 2023/10/17 07:32:58 by robernar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef LIBFT_H
+#define LIBFT_H
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int     ft_isalnum(int c);
+int	ft_isascii(int c);
+#endif
