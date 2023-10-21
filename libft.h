58642e2f51ee73018463e95a713fe5de0e620d3f
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2023-10-21 05:04:33 -0300

    Update header with new functions
    42

--- libft.h
+++ libft.h
@@ -6,14 +6,20 @@
 /*   By: robernar <robernar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 23:08:08 by robernar          #+#    #+#             */
-/*   Updated: 2023/10/17 07:32:58 by robernar         ###   ########.fr       */
+/*   Updated: 2023/10/20 00:21:16 by robernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef LIBFT_H
 #define LIBFT_H
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int     ft_isalnum(int c);
-int	ft_isascii(int c);
+typedef unsigned long	t_size;
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+void	*ft_memset(void *s, int c, t_size n);
+void	*ft_memcpy(void *dest, void *src, t_size n);
+void	ft_bzero(void *s, t_size n);
+t_size		ft_strlen(const char *s);
 #endif
