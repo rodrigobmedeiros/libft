db98aa17586f4a863e44110d63c1c8c703bf0cf9
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2023-10-21 05:06:04 -0300

    Implement new functions
    - ft_bzero.c
    - ft_isprint.c
    - ft_memcpy.c
    - ft_memset.c
    - ft_strlen.c
    
    42

---
+++ ft_memcpy.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: robernar <robernar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/20 00:18:19 by robernar          #+#    #+#             */
+/*   Updated: 2023/10/20 00:22:30 by robernar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+void	*ft_memcpy(void *dest, void *src, t_size n)
+{
+	unsigned char	*d;
+	unsigned char	*s;
+	t_size			i;
+
+	d = dest;
+	s = src;
+	i = 0;
+	while (i < n)
+	{
+		d[i] = s[i];
+		i++;
+	}
+	return (dest);
+}
