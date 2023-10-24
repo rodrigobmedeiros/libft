834bc3faefef72671750b02717c12c18660ba6a3
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2023-10-24 08:06:20 -0300

    Solve ft_lower

--- ft_tolower.c
+++ ft_tolower.c
@@ -1,23 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_toupper.c                                       :+:      :+:    :+:   */
+/*   ft_tolower.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: robernar <robernar@student.42.rj>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/24 07:00:54 by robernar          #+#    #+#             */
-/*   Updated: 2023/10/24 07:32:36 by robernar         ###   ########.fr       */
+/*   Created: 2023/10/24 07:24:51 by robernar          #+#    #+#             */
+/*   Updated: 2023/10/24 07:32:01 by robernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-int	is_lower(int c)
+int	is_upper(int c)
 {
-	return (c >= 'a' && c <= 'z');
+	return (c >= 'A' && c <= 'Z');
 }
 
-int	ft_toupper(int c)
+int	ft_tolower(int c)
 {
 	int	ascii_offset;
 
 	ascii_offset = 32;
-	return (c - is_lower(c) * ascii_offset);
+	return (c + is_upper(c) * ascii_offset);
 }
