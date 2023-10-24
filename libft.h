49f1ab8bc582100c30261393b7e3008dae05da65
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2023-10-24 08:50:06 -0300

    Solve ft_strchr

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: robernar <robernar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 23:08:08 by robernar          #+#    #+#             */
-/*   Updated: 2023/10/24 07:26:56 by robernar         ###   ########.fr       */
+/*   Updated: 2023/10/24 08:11:47 by robernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef LIBFT_H
@@ -24,6 +24,7 @@ void	*ft_memset(void *s, int c, t_size n);
 void	*ft_memcpy(void *dest, void *src, t_size n);
 void	*ft_memmove(void *dest, void *src, t_size n);
 void	ft_bzero(void *s, t_size n);
+char    *ft_strchr(const char *s, int c);
 t_size		ft_strlen(const char *s);
 t_size		ft_strlcpy(char *dst, const char *src, t_size size);
 t_size		ft_strlcat(char *dst, const char *src, t_size size);
