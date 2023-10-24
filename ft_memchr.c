7fafb4779048d8ee89cd0d3e629a67a3522ac401
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2023-10-24 19:03:12 -0300

    solve ft_memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,18 +6,23 @@
 /*   By: robernar <robernar@student.42.rj>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 09:31:25 by robernar          #+#    #+#             */
-/*   Updated: 2023/10/24 09:37:07 by robernar         ###   ########.fr       */
+/*   Updated: 2023/10/24 19:02:58 by robernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
-void	*ft_memchr(const void *s, int c, t_size n);
+void	*ft_memchr(const void *s, int c, t_size n)
+{
+	unsigned char	*char_s;
+	t_size			i;
 
-	char	*char_s;
-	t_size	i;
-
+	char_s = (unsigned char *)s;
 	i = 0;
 	while (i < n)
 	{
-		if (char_s[i] == )
+		if (char_s[i] == (unsigned char)c)
+			return ((void *)(char_s + i));
+		i++;
 	}
+	return ((void *)0);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: robernar <robernar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 23:08:08 by robernar          #+#    #+#             */
-/*   Updated: 2023/10/24 09:10:17 by robernar         ###   ########.fr       */
+/*   Updated: 2023/10/24 18:39:28 by robernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef LIBFT_H
@@ -24,6 +24,7 @@ int		ft_strncmp(const char *s1, const char *s2, t_size n);
 void	*ft_memset(void *s, int c, t_size n);
 void	*ft_memcpy(void *dest, void *src, t_size n);
 void	*ft_memmove(void *dest, void *src, t_size n);
+void    *ft_memchr(const void *s, int c, t_size n);
 void	ft_bzero(void *s, t_size n);
 char    *ft_strchr(const char *s, int c);
 char    *ft_strrchr(const char *s, int c);
