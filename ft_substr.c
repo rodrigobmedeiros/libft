9f6ddbf74527d0f785f7e4d998fd439a2e26fbef
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2023-10-26 18:40:12 -0300

    Solve substr

---
+++ ft_substr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: robernar <robernar@student.42.rj>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/26 18:06:54 by robernar          #+#    #+#             */
+/*   Updated: 2023/10/26 18:39:32 by robernar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, t_size len)
+{
+	char	*substr;
+	t_size	s_len;	
+	t_size	i;
+
+	i = 0;
+	s_len = ft_strlen(s);
+	substr = (char *)malloc(sizeof(char) * (len + 1));
+	if (!substr)
+		return ((void *)0);
+	while (*s && i < len)
+		substr[i++] = s[start++];
+	substr[i] = '\0';
+	return (substr);
+}
