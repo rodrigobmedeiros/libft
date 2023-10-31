6208468ddbbe0c04eeb80477c2f12b3d4bc1b1af
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2023-10-31 07:28:59 -0300

    Solve ft_strtrim

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: robernar <robernar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 06:43:19 by robernar          #+#    #+#             */
-/*   Updated: 2023/10/30 22:59:34 by robernar         ###   ########.fr       */
+/*   Updated: 2023/10/31 07:17:15 by robernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -14,19 +14,17 @@
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	char	*s;
+	char	*s_end;
 	char	*trimmed;
-	
+
 	trimmed = malloc((ft_strlen(s1) + 1) * sizeof(char));
 	if (!trimmed)
 		return ((void *)0);
 	s = (char *)s1;
+	s_end = (char *)(s1 + ft_strlen(s1));
 	while (ft_strchr(set, *s))
 		s++;
-	return (ft_strsubs(s1, s - s1, 20);
-}
-
-#include <stdio.h>
-int main()
-{
-	printf("%s", "abrodrigoab", "ab");
+	while (ft_strrchr(set, *s_end))
+		s_end--;
+	return (ft_substr(s1, s - s1, s_end - s + 1));
 }
--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,23 +6,21 @@
 /*   By: robernar <robernar@student.42.rj>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 18:06:54 by robernar          #+#    #+#             */
-/*   Updated: 2023/10/28 06:40:51 by robernar         ###   ########.fr       */
+/*   Updated: 2023/10/31 06:58:20 by robernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 char	*ft_substr(char const *s, unsigned int start, t_size len)
 {
-	char	*substr;
-	t_size	s_len;	
+	char	*substr;	
 	t_size	i;
 
 	i = 0;
-	s_len = ft_strlen(s);
 	substr = (char *)malloc(sizeof(char) * (len + 1));
 	if (!substr)
 		return ((void *)0);
-	while (start < (unsigned int)s_len && i < len)
+	while (s[start] && i < len)
 		substr[i++] = s[start++];
 	substr[i] = '\0';
 	return (substr);
