f1e0a2524f2b4dac714aab3eb92e2dc52207e42a
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2023-11-05 19:56:06 -0300

    Adjust to deal with string composed only by char to trim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: robernar <robernar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 06:43:19 by robernar          #+#    #+#             */
-/*   Updated: 2023/10/31 07:17:15 by robernar         ###   ########.fr       */
+/*   Updated: 2023/11/05 19:53:00 by robernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -22,9 +22,9 @@ char	*ft_strtrim(char const *s1, char const *set)
 		return ((void *)0);
 	s = (char *)s1;
 	s_end = (char *)(s1 + ft_strlen(s1));
-	while (ft_strchr(set, *s))
+	while (ft_strchr(set, *s) && *s)
 		s++;
-	while (ft_strrchr(set, *s_end))
+	while (ft_strrchr(set, *s_end) && s_end > s)
 		s_end--;
 	return (ft_substr(s1, s - s1, s_end - s + 1));
 }
