7c1fa729c8b1c1745b4b3893e1d5839908190f4b
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2023-11-09 16:22:16 -0300

    Deal with case where both string finish at the same time

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: robernar <robernar@student.42.rj>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 09:08:10 by robernar          #+#    #+#             */
-/*   Updated: 2023/10/24 09:29:20 by robernar         ###   ########.fr       */
+/*   Updated: 2023/11/09 16:21:37 by robernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -18,7 +18,7 @@ int	ft_strncmp(const char *s1, const char *s2, t_size n)
 	if (n <= 0)
 		return (0);
 	i = 0;
-	while (s1[i] == s2[i] && i < n - 1)
+	while (s1[i] == s2[i] && i < n - 1 && s1[i])
 		i++;
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
