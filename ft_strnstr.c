2ca1aba597b71cac830ed5357baaa0151d63ff80
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2023-11-09 18:10:15 -0300

    solve ft_strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: robernar <robernar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 05:26:13 by robernar          #+#    #+#             */
-/*   Updated: 2023/10/25 06:35:44 by robernar         ###   ########.fr       */
+/*   Updated: 2023/11/09 17:49:50 by robernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -18,10 +18,14 @@ char	*ft_strnstr(const char *big, const char *little, t_size len)
 	little_len = ft_strlen(little);
 	if (little_len == 0)
 		return ((char *)(big));
-	while (len-- > little_len)
+	if (len == 0)
+		return (NULL);
+	if (!*big)
+		return (NULL);
+	while (len > 0 && len-- >= little_len)
 	{
 		if (!ft_strncmp(big++, little, little_len))
 			return ((char *)(big - 1));
 	}
-	return ((void *)0);
+	return (NULL);
 }
