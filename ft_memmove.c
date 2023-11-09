cd4a9c0056728ad696c973c9f42885b7cda499dd
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2023-11-09 17:42:59 -0300

    Adjust memmove and strlcat

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,10 +6,22 @@
 /*   By: robernar <robernar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/21 05:06:36 by robernar          #+#    #+#             */
-/*   Updated: 2023/11/09 16:17:15 by robernar         ###   ########.fr       */
+/*   Updated: 2023/11/09 17:40:07 by robernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
+
+int	ft_cpy_char_down(unsigned char *d, unsigned char *s, t_size i)
+{
+	d[i] = s[i];
+	return (--i);
+}
+
+int	ft_cpy_char_up(unsigned char *d, unsigned char *s, t_size i)
+{
+	d[i] = s[i];
+	return (++i);
+}
 
 void	*ft_memmove(void *dest, void *src, t_size n)
 {
@@ -26,17 +38,13 @@ void	*ft_memmove(void *dest, void *src, t_size n)
 	{
 		i = n - 1;
 		while (i >= 0)
-		{
-			d[i] = s[i];
-			i--;
-		}
+			i = ft_cpy_char_down(d, s, i);
 	}
 	else
 	{
 		while (i < n)
 		{
-			d[i] = s[i];
-			i++;
+			i = ft_cpy_char_up(d, s, i);
 		}
 	}
 	return (dest);
