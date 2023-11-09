801e8cae1912615ddc5e06c632a8b67555cca91a
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2023-11-09 18:19:08 -0300

    Solve calloc again

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,15 +6,15 @@
 /*   By: robernar <robernar@student.42.rj>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 07:54:30 by robernar          #+#    #+#             */
-/*   Updated: 2023/11/09 18:08:43 by robernar         ###   ########.fr       */
+/*   Updated: 2023/11/09 18:11:15 by robernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 void	*ft_calloc(t_size nmemb, t_size size)
 {
-	int	total_size;
 	void	*allocated;
+	int		total_size;
 
 	total_size = (int)nmemb * (int)size;
 	if (total_size / size != nmemb)
