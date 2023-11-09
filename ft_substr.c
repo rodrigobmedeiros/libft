517d18fb46f913848215c72cd09a6842547db872
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2023-11-09 18:19:27 -0300

    Solve ft_substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: robernar <robernar@student.42.rj>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 18:06:54 by robernar          #+#    #+#             */
-/*   Updated: 2023/10/31 06:58:20 by robernar         ###   ########.fr       */
+/*   Updated: 2023/11/09 18:18:52 by robernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -15,12 +15,14 @@ char	*ft_substr(char const *s, unsigned int start, t_size len)
 {
 	char	*substr;	
 	t_size	i;
+	t_size	s_len;
 
+	s_len = ft_strlen(s);
 	i = 0;
 	substr = (char *)malloc(sizeof(char) * (len + 1));
 	if (!substr)
-		return ((void *)0);
-	while (s[start] && i < len)
+		return (NULL);
+	while (s[start] && i < len && start <= s_len)
 		substr[i++] = s[start++];
 	substr[i] = '\0';
 	return (substr);
