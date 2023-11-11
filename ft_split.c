e5f0d36594c4809230923579e440c2290eee4452
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2023-11-11 12:29:55 -0300

    Adjust treatment for mallocs

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: robernar <robernar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/05 07:13:39 by robernar          #+#    #+#             */
-/*   Updated: 2023/11/11 12:00:13 by robernar         ###   ########.fr       */
+/*   Updated: 2023/11/11 12:22:39 by robernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -51,6 +51,8 @@ static char	*ft_add_delimiter_to_end(char const *s, char c)
 
 	len = ft_strlen(s);
 	cpy_s = (char *)malloc(sizeof(char) * (len + 2));
+	if (!cpy_s)
+		return (NULL);
 	ft_strlcpy(cpy_s, s, len + 1);
 	cpy_s[len] = c;
 	cpy_s[len + 1] = '\0';
@@ -86,6 +88,8 @@ char	**ft_split(char const *s, char c)
 		return (NULL);
 	i = 0;
 	cpy_s = ft_add_delimiter_to_end(s, c);
+	if (!cpy_s)
+		return (NULL);
 	start = cpy_s;
 	cpy_s = ft_move_pointer_to_next_non_delimiter(cpy_s, c);
 	n_words = ft_count_words(s, c);
